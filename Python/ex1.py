e6ead59d117de45798da2d933086639f0d9b8cc6
TaeoNeX <john@ickle.co.uk>
2015-02-04 22:49:36 +0000

    Ruby+Python

---
+++ Python/ex1.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Hello Lara, how are you?"
+print 'The next line is "commented" out'
+# print 'If this works you won't see this'
